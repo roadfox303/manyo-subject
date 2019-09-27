e1edc2a972bd8b1b2c0f227ee1b7692af4f5696b
takamori nanbu <roadfox303@gmail.com>
2019-09-27 11:40:23 +0900

    ユーザー、ログイン、ログアウト実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,5 @@ states = [
   { progress:'完了'}
 ]
 State.create! states
-users = [
-  { name: 'テストユーザー', email: 'test@gmail.com', password_digest: 'password333'}
-]
+users = { name: 'テストユーザー', email: 'test@gmail.com', password: 'password333'}
 User.create! users
