33cb0a2d89cb285c4a58511c329b45eefb455f5b
takamori nanbu <roadfox303@gmail.com>
2019-09-30 19:48:52 +0900

    seedファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,7 @@ states = [
   { progress:'完了'}
 ]
 State.create! states
-users = [{ name: 'Admin', email: 'admin@gmail.com', password: 'superadmin'},
-  { name: 'テストユーザー', email: 'test@gmail.com', password: 'password333'}
-]
+users = { name: 'Admin', email: 'admin@gmail.com', password: 'superadmin'}
 User.create! users
 admin = { user_id: 1}
 Admin.create! admin
